2b176d6ff01d5ea941418ae38aa1ecd51603d5c0
Mano Sriram <mano.sriram0@gmail.com>
2018-09-21 20:05:46 +0530

    Added TTT

--- ttt.cpp
+++ ttt.cpp
@@ -3,157 +3,170 @@ using namespace std;
 
 // Initializations....
 static char grid[3][3];
-static char one = grid[0][0];
-static char two = grid[0][1];
-static char three = grid[0][2];
-static char four = grid[1][0];
-static char five = grid[1][1];
-static char six = grid[1][2];
-static char seven = grid[2][0];
-static char eight = grid[2][1];
-static char nine = grid[2][2];
-static count;
+static char one = grid[0][0] = '1';
+static char two = grid[0][1] = '2';
+static char three = grid[0][2] = '3';
+static char four = grid[1][0] = '4';
+static char five = grid[1][1] = '5';
+static char six = grid[1][2] = '6';
+static char seven = grid[2][0] = '7';
+static char eight = grid[2][1] = '8';
+static char nine = grid[2][2] = '9';
+// static int count;
 
-void takeMove(int pm,int n) {
+void takeMove(int pm, int n)
+{
 
-    switch(pm) {
-        
-        case 1:
+    switch (pm)
+    {
 
-        if (n==1)
-        one = 'X';
+    case 1:
 
-        if (n==2)
-        one = 'O';
+        if (n == 1)
+            grid[0][0] = 'X';
+
+        if (n == 2)
+            grid[0][0] = 'O';
 
         break;
 
-        case 2:
+    case 2:
 
-        if (n==1)
-        two = 'X';
+        if (n == 1)
+            grid[0][1] = 'X';
 
-        else if (n==2)
-        two = 'O';
+        else if (n == 2)
+            grid[0][1] = 'O';
         break;
 
-        case 3:
-        if (n==1)
-        three = 'X';
+    case 3:
+        if (n == 1)
+            grid[0][2] = 'X';
 
-        else if (n==2)
-        three = 'O';
+        else if (n == 2)
+            grid[0][2] = 'O';
         break;
 
-        case 4:
-        if (n==1)
-        four = 'X';
+    case 4:
+        if (n == 1)
+            grid[1][0] = 'X';
 
-        else if (n==2)
-        four = 'O';
+        else if (n == 2)
+            grid[1][0] = 'O';
         break;
 
-        case 5:
-        if (n==1)
-        five = 'X';
+    case 5:
+        if (n == 1)
+            grid[1][1] = 'X';
 
-        else if (n==2)
-        five = 'O';
+        else if (n == 2)
+            grid[1][1] = 'O';
         break;
 
-        case 6:
-        if (n==1)
-        six = 'X';
-        else if (n==2)
-        six = 'O';
+    case 6:
+        if (n == 1)
+            grid[1][2] = 'X';
+        else if (n == 2)
+            grid[1][2] = 'O';
         break;
 
-        case 7:
-        if (n==1)
-        seven = 'X';
-        else if (n==2)
-        seven = 'O';
+    case 7:
+        if (n == 1)
+            grid[2][0] = 'X';
+        else if (n == 2)
+            grid[2][0] = 'O';
         break;
 
-        case 8:
-        if (n==1)
-        eight = 'X';
-        else if (n==2)
-        eight = 'O';
+    case 8:
+        if (n == 1)
+            grid[2][1] = 'X';
+        else if (n == 2)
+            grid[2][1] = 'O';
         break;
 
-        case 9:
-        if (n==1)
-        nine = 'X';
-        else if (n==2)
-        nine = 'O';
+    case 9:
+        if (n == 1)
+            grid[2][2] = 'X';
+        else if (n == 2)
+            grid[2][2] = 'O';
         break;
     }
-    count++;
 }
 
-bool checkStatus() {
-    if (one == two && two == three)
-    return 1;
+bool checkStatus()
+{
 
-    else if (four == five && five == six)
-    return 1;
+    if (grid[0][0] == grid[0][1] && grid[0][1] == grid[0][2])
+        return 1;
 
-    else if (seven == eight && eight == nine)
-    return 1;
+    if (grid[1][0] == grid[1][1] && grid[1][1] == grid[1][2])
+        return 1;
 
-    else if (one == four && four == seven)
-    return 1;
+    if (grid[2][0] == grid[2][1] && grid[2][1] == grid[2][2])
+        return 1;
 
-    else if (two == five && five == eight)
-    return 1;
+    if (grid[0][0] == grid[1][0] && grid[1][0] == grid[2][0])
+        return 1;
 
-    else if (three == six && six == nine)
-    return 1;
+    if (grid[0][1] == grid[1][1] && grid[1][1] == grid[2][1])
+        return 1;
 
-    else if (one == five && five == nine)
-    return 1;
+    if (grid[0][2] == grid[1][2] && grid[1][2] == grid[2][2])
+        return 1;
 
-    else if (three == five && five == seven)
-    return 1;
+    if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2])
+        return 1;
+
+    if (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0])
+        return 1;
 
     else
-    return 0;
+        return 0;
 }
 
+int main()
+{
 
-int main() {
-    
-    int pm,n;
+    int pm, n;
 
-    while(count<=9) {
-
-        for (int i=0;i<3;i++) {
-            for (int j=0;j<3;j++) {
-                cout << grid[i][j] << " ";
-            }
-            cout << '\n';
-        }
-
+    while (1)
+    {
+        cout << '\n';
+        cout << '|' << " " << grid[0][0] << " " << '|' << " " << grid[0][1] << " " << '|' << " " << grid[0][2] << " " << '|' << " " << '\n';
+        cout << "--------------" << '\n';
+        cout << '|' << " " << grid[1][0] << " " << '|' << " " << grid[1][1] << " " << '|' << " " << grid[1][2] << " " << '|' << " " << '\n';
+        cout << "--------------" << '\n';
+        cout << '|' << " " << grid[2][0] << " " << '|' << " " << grid[2][1] << " " << '|' << " " << grid[2][2] << " " << '|' << " " << '\n';
+        cout << '\n';
 
         cout << "Enter Player 1's Move : (1-9) " << '\n';
         cin >> pm;
-        n=1;
-        takeMove(pm,n);
+        n = 1;
+        takeMove(pm, n);
 
-        if (checkStatus()) {
-        cout << "Player One Wins!!" << '\n';
-        exit(0);
+        if (checkStatus() == 1)
+        {
+            cout << "Player ONE Wins!!" << '\n';
+            exit(0);
         }
+
+        cout << '\n';
+        cout << '|' << " " << grid[0][0] << " " << '|' << " " << grid[0][1] << " " << '|' << " " << grid[0][2] << " " << '|' << " " << '\n';
+        cout << "--------------" << '\n';
+        cout << '|' << " " << grid[1][0] << " " << '|' << " " << grid[1][1] << " " << '|' << " " << grid[1][2] << " " << '|' << " " << '\n';
+        cout << "--------------" << '\n';
+        cout << '|' << " " << grid[2][0] << " " << '|' << " " << grid[2][1] << " " << '|' << " " << grid[2][2] << " " << '|' << " " << '\n';
+        cout << '\n';
 
         cout << "Enter Player 2's Move : (1-9) " << '\n';
         cin >> pm;
-        n=2;
-        takeMove(pm,n);
+        n = 2;
+        takeMove(pm, n);
 
-        if (checkStatus()) {
-        cout << "Player Two Wins!!" << '\n';
-        exit(0);
+        if (checkStatus())
+        {
+            cout << "Player TWO Wins!!" << '\n';
+            exit(0);
         }
     }
 }
