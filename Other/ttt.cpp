52b35b71202b485e844a77be6d0f4ee49b69e466
Mano Sriram <mano.sriram0@gmail.com>
2018-09-22 16:16:20 +0530

    added a folder and facebook app data...

--- Other/ttt.cpp
+++ Other/ttt.cpp
@@ -233,7 +233,7 @@ void resetGrids()
     doneEight = false;
     doneNine = false;
 }
-
+// Checks if all cells are not equal...used when a match is // tied...
 bool stat()
 {
     if (grid[0][0] != '1' && grid[0][1] != '2' && grid[0][2] != '3' && grid[1][0] != '4' && grid[1][1] != '5' && grid[1][2] != '6' && grid[2][0] != '7' && grid[2][1] != '8' && grid[2][2] != '9')
