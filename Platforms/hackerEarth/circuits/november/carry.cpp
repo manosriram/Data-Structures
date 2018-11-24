96d58849b69b75002c9227c6d95ebc9ee8e2ec6a
Mano Sriram <mano.sriram0@gmail.com>
2018-11-24 20:38:05 +0530

    completed addition errors.

--- Platforms/hackerEarth/circuits/november/carry.cpp
+++ Platforms/hackerEarth/circuits/november/carry.cpp
@@ -1,39 +1,29 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
-int getCarry(int n, int m)
+unsigned long long int  getCarry(unsigned long long int n, unsigned long long int m)
 {
     unsigned long long int res, multiplier = 1, bitSet, result = 0;
-    if ((n % 10) + (n % 10) > 9)
-    {
-        while (n || m)
+          while (n || m)
         {
 
             res = (n % 10) + (m % 10);
             bitSet = res % 10;
-            bitSet *= multiplier;
-            result += bitSet;
+            bitSet *= multiplier % mod;
+            result += bitSet % mod;
             n /= 10;
             m /= 10;
             multiplier *= 10;
         }
-    }
-    else
-    {
-        return n + m;
-    }
-
-    return result;
+            return result;
 }
 
 int main()
 {
     int T;
-    unsigned long int mod = 1000000009;
     unsigned long long int n, m;
     unsigned long long int result, realResult;
-    
+
     cin >> T;
 
     while (T--)
@@ -41,6 +31,6 @@ int main()
         cin >> n >> m;
         result = getCarry(n, m);
         realResult = (n + m);
-        cout << abs(result - realResult) << endl;
+        cout << -(result - realResult) << endl;
     }
 }
