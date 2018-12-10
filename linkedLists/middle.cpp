86df24f2c31437a0c0a3368c7fa5018f409e68d0
Mano Sriram <mano.sriram0@gmail.com>
2018-12-11 00:21:13 +0530

    added linked lists..

---
+++ linkedLists/middle.cpp
@@ -0,0 +1,6 @@
+#include "single.h"
+using namespace std;
+
+int main()
+{
+}
--- linkedLists/single.cpp
+++ linkedLists/single.cpp
@@ -59,6 +59,74 @@ struct node *insertAtStart(struct node *start, int data)
     return start;
 }
 
+int findMiddle(struct node *start)
+{
+    node *p;
+    p = start;
+    int count = 0;
+    while (p->next != NULL)
+    {
+        p = p->next;
+        count++;
+    }
+    int i = 0;
+    p = start;
+    while (i != count / 2)
+    {
+        p = p->next;
+        i++;
+    }
+    return p->data;
+}
+
+void deleteMiddleElement(struct node *start)
+{
+    node *p, *q;
+    p = start;
+    int count = 0;
+    while (p->next != NULL)
+    {
+        p = p->next;
+        count++;
+    }
+    int i = 0;
+    p = start;
+    while (i != count / 2)
+    {
+        q = p;
+        p = p->next;
+        i++;
+    }
+    q->next = p->next;
+    delete p;
+}
+
+void deleteLastElement(struct node *start)
+{
+    node *p, *q;
+    p = start;
+
+    while (p->next != NULL)
+    {
+        q = p;
+        p = p->next;
+    }
+    delete p;
+    q->next = NULL;
+    cout << "Last Element Deleted!!" << endl;
+    return;
+}
+
+struct node *deleteFirstElement(struct node *start)
+{
+    node *p;
+    p = start->next;
+    delete start;
+    start = p;
+    cout << "First Element Deleted!!" << endl;
+    return start;
+}
+
 int main()
 {
     node *start;
@@ -68,6 +136,22 @@ int main()
     insertNodeAtEnd(start, 91);
     insertNodeAtEnd(start, 41);
     insertNodeAtEnd(start, 67);
+    insertNodeAtEnd(start, 123);
+    insertNodeAtEnd(start, 3232);
     start = insertAtStart(start, 3412);
     displayNodes(start);
+    cout << endl;
+    findMiddle(start);
+    deleteMiddleElement(start);
+    cout << "After deleting Middle Element : " << endl;
+    cout << endl;
+    displayNodes(start);
+    deleteLastElement(start);
+    cout << "After deleting Last Element : " << endl;
+    cout << endl;
+    displayNodes(start);
+    start = deleteFirstElement(start);
+    cout << "After Deleting First Element : " << endl;
+    displayNodes(start);
+    cout << endl;
 }
