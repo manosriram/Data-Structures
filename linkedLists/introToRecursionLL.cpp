f81f02423fc020d9cd1ceac1ee846d6cc2365e0b
Mano Sriram <mano.sriram0@gmail.com>
2019-04-07 21:23:52 +0530

    Added Inorder through Recursion.

---
+++ linkedLists/introToRecursionLL.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "basicActions.h"
+using namespace std;
+
+Node *recDis(Node *head)
+{
+    if (!head)
+        return NULL;
+
+    cout << head->data << " ";
+    recDis(head->next);
+}
+
+int main()
+{
+    Node *head = NULL;
+    head = createNode(head, 10);
+    insertAtEnd(head, 20);
+    insertAtEnd(head, 30);
+    insertAtEnd(head, 40);
+    insertAtEnd(head, 50);
+
+    recDis(head);
+}
