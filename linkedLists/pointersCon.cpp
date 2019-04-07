8b7ab463792696a1131392bb621f3ce74aa11ffd
Mano Sriram <mano.sriram0@gmail.com>
2019-04-08 01:27:47 +0530

    Added Remove Node from List without head pointer given.

---
+++ linkedLists/pointersCon.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "basicActions.h"
+using namespace std;
+
+void deleteWithoutHead(Node *ptr)
+{
+    *ptr = *ptr->next;
+}
+
+int main()
+{
+    Node *head = NULL;
+
+    head = createNode(head, 10);
+    insertAtEnd(head, 20);
+    insertAtEnd(head, 4);
+    insertAtEnd(head, 30);
+
+    deleteWithoutHead(head->next);
+    displayNodes(head);
+}
