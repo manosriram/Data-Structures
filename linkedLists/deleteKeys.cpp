ea8f9b6774dbd5e918b7ef4c328e4855fdbb9d92
Mano Sriram <mano.sriram0@gmail.com>
2019-04-27 23:01:16 +0530

    Added docs.

--- linkedLists/deleteKeys.cpp
+++ linkedLists/deleteKeys.cpp
@@ -2,6 +2,19 @@
 #include "basicActions.h"
 using namespace std;
 
+/*
+Function to Delete Keys.
+We can have 3 base cases :
+
+1. If the Key is at the Start of the List, Remove Element at Start.
+2. If the Key is at the End of the List, Remove Element at End.
+3. If the Key is somewhere in the middle :
+    -> Set Prev Node's (q) "next" to Current Node's (p) "next".
+    -> Delete Current Node.
+    -> Set Current Node to Prev's "next".
+
+*/
+
 Node *deleteKeys(Node *head, int x)
 {
     Node *p = head, *q = NULL;
