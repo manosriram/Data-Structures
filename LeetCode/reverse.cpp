09093777516fc710bfdf06b18db23b69cf55dc33
Mano Sriram <mano.sriram0@gmail.com>
2019-06-16 02:04:47 +0530

    Added Root to Leaf Paths.

--- LeetCode/reverse.cpp
+++ LeetCode/reverse.cpp
@@ -3,40 +3,43 @@
 
 using namespace std;
 
-int reverse(int x) {
+int reverse(int x)
+{
 
-  if (x >= INT_MAX) return 0;
+  if (x >= INT_MAX)
+    return 0;
 
   bool neg;
   int n;
   (x < 0) ? neg = true : neg = false;
   int m = n = abs(x);
-  int count=0;
+  int count = 0;
 
-  while (n > 0) {
+  while (n > 0)
+  {
     count++;
-    n/=10;
+    n /= 10;
   }
 
-  int temp=0;
+  int temp = 0;
   // int ind=count;
 
-  while (count--) {
-    temp += (m%10) * pow(10,count);
+  while (count--)
+  {
+    temp += (m % 10) * pow(10, count);
     m /= 10;
   }
 
   (neg) ? temp = temp - (temp * 2) : temp;
-  
+
   return temp;
 }
 
-
-int main() {
+int main()
+{
   int n;
 
   cin >> n;
 
   cout << reverse(n) << endl;
-
 }
