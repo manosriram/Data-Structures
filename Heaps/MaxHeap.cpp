bfef6783d20a3bdb33f9ab81ca0b59fa546ee990
Mano Sriram <mano.sriram0@gmail.com>
2019-07-26 20:52:30 +0530

    Finished HeapSort.

--- Heaps/MaxHeap.cpp
+++ Heaps/MaxHeap.cpp
@@ -8,70 +8,87 @@ struct Heap
     int maxHeapSize;
 };
 
-void maxHeapify(Heap *hp, int i, int n)
+void heapifyDown(Heap *heap, int i, int n)
 {
     int largest = i;
-
     int left = 2 * i;
     int right = 2 * i + 1;
 
-    if (left <= n && hp->heapArr[left] > hp->heapArr[largest])
+    if (left <= n && heap->heapArr[left] > heap->heapArr[largest])
         largest = left;
 
-    if (right <= n && hp->heapArr[right] > hp->heapArr[largest])
+    if (right <= n && heap->heapArr[right] > heap->heapArr[largest])
         largest = right;
 
     if (largest != i)
     {
-        int temp = hp->heapArr[largest];
-        hp->heapArr[largest] = hp->heapArr[i];
-        hp->heapArr[i] = hp->heapArr[largest];
-
-        maxHeapify(hp, largest, n);
+        swap(heap->heapArr[i], heap->heapArr[largest]);
+        heapifyDown(heap, largest, n);
     }
     return;
 }
 
-void buildHeap(Heap *hp, int n)
+void insert(Heap *heap, int key)
 {
-    for (int t = n / 2; t >= 0; t--)
-    {
-        maxHeapify(hp, t, n);
-    }
+    heap->heapArr[++heap->currentHeapSize] = key;
+    return;
 }
 
-void insertKey(Heap *&hp, int key)
+void buildHeap(Heap *heap, int n)
 {
-    hp->heapArr[++hp->currentHeapSize] = key;
+    for (int t = n / 2; t >= 1; t--)
+    {
+        heapifyDown(heap, t, n);
+    }
+    return;
+}
+
+int extractMax(Heap *&heap)
+{
+    int root = heap->heapArr[1];
+    
+    swap(heap->heapArr[1], heap->heapArr[heap->currentHeapSize]);
+    
+    heap->currentHeapSize--;
+
+    heapifyDown(heap, 1, heap->currentHeapSize);
+    return root;
+}
+
+void printHeap(Heap *heap, int n) {
+    for (int t=1;t<=n;t++)
+        cout << heap->heapArr[t] << " ";
+    
+    cout << endl;
+    return;
+}
+
+void heapSort(Heap *heap) {
+    int n = heap->currentHeapSize;
+    for (int t = 1;t<=n;t++) {
+        extractMax(heap);
+    }
+    printHeap(heap, n);
     return;
 }
 
 int main()
 {
-    Heap *hp = new Heap();
-    hp->currentHeapSize = 0;
-    hp->maxHeapSize = 64;
-    hp->heapArr = new int[hp->maxHeapSize];
+    Heap *heap = new Heap();
+    heap->currentHeapSize = 0;
+    heap->maxHeapSize = 64;
+    heap->heapArr = new int[heap->maxHeapSize];
 
-    buildHeap(hp, hp->currentHeapSize);
+    insert(heap, 1);
+    insert(heap, 4);
+    insert(heap, 5);
+    insert(heap, 90);
+    insert(heap, -90);
+    insert(heap, -40);
+    insert(heap, 3);
 
-    insertKey(hp, 4);
-    insertKey(hp,1);
-    insertKey(hp, 2);
-    insertKey(hp, 90);
+    int size = heap->currentHeapSize;
+    buildHeap(heap, size);
 
-    int size = hp->currentHeapSize;
-    for (int i = size; i>=2;i--) {
-        int temp = hp->heapArr[1];
-        hp->heapArr[1] = hp->heapArr[i];
-        hp->heapArr[i] = temp;
-
-        --hp->currentHeapSize;
-        maxHeapify(hp, i, size);
-    }
-
-    for (int t = 1 ;t<size;t++)
-        cout << hp->heapArr[t] << " ";
-
-
+    heapSort(heap);
 }
