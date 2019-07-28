09e3663da3e7d98cb2d055b78227b56796193d3e
Mano Sriram <mano.sriram0@gmail.com>
2019-07-28 22:57:47 +0530

    Add DFS.

--- Graphs/BFS.cpp
+++ Graphs/BFS.cpp
@@ -6,19 +6,26 @@ using namespace std;
 void BFS(vector<int> grph[], int root, bool visited[])
 {
     queue<int> queue;
-    visited[root] = true;
-    queue.push(root);
 
+    // Push the Root and mark it as visited.
+    queue.push(root);
+    visited[root] = true;
+
+    // While the Queue isn't Empty.
     while (!queue.empty())
     {
+        // Pop the front element and print it.
         int frnt = queue.front();
         cout << frnt << " ";
         queue.pop();
 
+        // For the Popped element, get the nodes they are connected to.
         for (auto t = grph[frnt].begin(); t != grph[frnt].end(); t++)
         {
+            // Push the element into queue if-and-only-if it is not visited.
             if (!visited[*t])
             {
+                // Mark the node as Visited and push the element into queue.
                 visited[*t] = true;
                 queue.push(*t);
             }
---
+++ Graphs/DFS.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include "Intro.hpp"
+using namespace std;
+
+void DFS(int vtx, bool visited[], vector<int> grph[])
+{
+    visited[vtx] = true;
+    cout << vtx << " ";
+
+    for (auto itr = grph[vtx].begin(); itr != grph[vtx].end(); itr++)
+    {
+        if (!visited[*itr])
+        {
+            DFS(*itr, visited, grph);
+        }
+    }
+    return;
+}
+
+int main()
+{
+    int nodes = 9;
+    vector<int> grph[10];
+    bool visited[nodes];
+    memset(visited, false, sizeof(visited));
+
+    addEdge(1, 2, grph);
+    addEdge(1, 3, grph);
+    addEdge(2, 4, grph);
+    addEdge(2, 5, grph);
+    addEdge(2, 6, grph);
+    addEdge(3, 7, grph);
+    addEdge(7, 8, grph);
+    addEdge(8, 9, grph);
+
+    DFS(1, visited, grph);
+    cout << endl;
+}
