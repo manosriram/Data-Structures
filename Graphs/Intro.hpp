e92e985ca9eb7e657403bad59c11de5e13490b61
Mano Sriram <mano.sriram0@gmail.com>
2019-07-31 21:21:09 +0530

    Add Number of Ways to reach from src to dest.

--- Graphs/Intro.hpp
+++ Graphs/Intro.hpp
@@ -6,8 +6,8 @@ using namespace std;
 template <class T>
 void addEdge(T src, T dest, vector<int> grph[])
 {
-    grph[src - 'a'].push_back(dest);
-    grph[dest - 'a'].push_back(src);
+    grph[src].push_back(dest);
+    grph[dest].push_back(src);
     return;
 }
 
@@ -15,10 +15,10 @@ void printGraph(vector<int> grph[], int nde)
 {
     for (int t = 0; t < nde; t++)
     {
-        cout << "Vertex " << static_cast<char>(t + 97);
+        cout << "Vertex " << t;
         for (auto nd : grph[t])
         {
-            cout << " -- " << static_cast<char>(nd);
+            cout << " -- " << nd;
         }
         cout << endl;
     }
--- Graphs/noOfWays.cpp
+++ Graphs/noOfWays.cpp
@@ -3,6 +3,8 @@
 #include <queue>
 #include "Intro.hpp"
 using namespace std;
+int pathCount = 0;
+vector<int> path[26];
 
 int findWays(vector<int> grph[], bool visited[], int src, int dest)
 {
@@ -15,20 +17,29 @@ int findWays(vector<int> grph[], bool visited[], int src, int dest)
     while (!qt.empty())
     {
         int root = qt.front();
+        visited[root] = true;
         qt.pop();
 
-        for (auto i = grph[root].begin(); i != grph[root].end(); i++)
+        if (root == dest)
         {
-            if (root = dest)
-                break;
-
-            if (*i == dest)
-                ways++;
-
-            if (!visited[*i])
+            visited[root] = true;
+            continue;
+        }
+        else
+        {
+            for (auto i = grph[root].begin(); i != grph[root].end(); i++)
             {
-                visited[*i] = true;
-                qt.push(*i);
+                if (*i == dest)
+                {
+                    ways++;
+                    pathCount++;
+                }
+
+                if (!visited[*i])
+                {
+                    visited[*i] = true;
+                    qt.push(*i);
+                }
             }
         }
     }
@@ -42,18 +53,18 @@ int main()
     bool visited[nodes];
     memset(visited, false, sizeof(visited));
 
-    addEdge('b', 'a', grph);
-    addEdge('a', 'd', grph);
-    addEdge('a', 'e', grph);
-    addEdge('a', 'c', grph);
-    addEdge('d', 'f', grph);
-    addEdge('d', 'i', grph);
-    addEdge('d', 'e', grph);
-    addEdge('i', 'e', grph);
-    addEdge('e', 'h', grph);
-    addEdge('e', 'c', grph);
-    addEdge('c', 'g', grph);
+    addEdge(1, 0, grph);
+    addEdge(0, 3, grph);
+    addEdge(0, 4, grph);
+    addEdge(0, 2, grph);
+    addEdge(3, 5, grph);
+    addEdge(3, 8, grph);
+    addEdge(3, 4, grph);
+    addEdge(8, 4, grph);
+    addEdge(4, 7, grph);
+    addEdge(4, 2, grph);
+    addEdge(2, 6, grph);
 
     // printGraph(grph, nodes);
-    // cout << findWays(grph, visited, ) << endl;
+    cout << findWays(grph, visited, 1, 4) << endl;
 }
