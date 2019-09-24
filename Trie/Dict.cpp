a61406e3eb8d52aa97064e84a84b219a0cd791eb
Mano Sriram <mano.sriram0@gmail.com>
2019-09-24 06:07:34 +0530

    Finish Dictionary.

--- Trie/Dict.cpp
+++ Trie/Dict.cpp
@@ -2,8 +2,50 @@
 #include "./BaseTemplate.hpp"
 using namespace std;
 
+void takeUp(TrieNode *root, string prefix) {
+
+    if (root->eOF)
+        cout << prefix << " ";
+    
+    if (isLastNode(root))
+        return;
+    
+    for (int t=0;t<26;t++) {
+        if (root->children[t]) {
+            prefix.push_back(97 + t);
+            takeUp(root->children[t], prefix);
+            prefix.pop_back();
+        }
+    }
+    return;
+}
+
+void displayContacts(TrieNode *root, string qry) {
+    int n = qry.length(), level;
+    TrieNode *crawl = root;
+    string word;
+
+    for (level=0;level<n;level++) {
+        int ind = qry[level] - 'a';
+        if (!crawl->children[ind])
+            return;
+
+        word.push_back(97 + ind);
+        cout << "Suggestions for " << word << " ";
+        crawl = crawl->children[ind];
+        takeUp(crawl, word);
+        cout << endl;
+    }
+
+}
+
 int main() {
     TrieNode *root = new TrieNode();
     root = createNode();
-    insert("mano", root);
+    insert("gforgeeks", root);
+    insert("geeksquiz", root);
+    insert("gamble", root);
+    insert("gallow", root);
+
+    displayContacts(root, "ge");
 }
