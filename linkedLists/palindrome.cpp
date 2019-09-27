53cd49e0933c9244bc25f70a667c2ffb2a41ffb5
Mano Sriram <mano.sriram0@gmail.com>
2019-09-27 22:41:50 +0530

    Start Efficient Palindrome.

--- linkedLists/palindrome.cpp
+++ linkedLists/palindrome.cpp
@@ -2,35 +2,57 @@
 #include "basicActions.h"
 using namespace std;
 
-bool isPalindrome(Node *head){
+bool isPalindromeEff(Node *head) {
+    Node *slowP = head, *fastP = head;
+
+    while (fastP) {
+        slowP = slowP->next;
+        fastP = fastP->next->next;
+    }
+    Node *checkP = head;
+    slowP = slowP->next;
+    // cout << slowP->data << endl;
+    while (slowP) {
+        if (checkP->data != slowP->data)
+            return false;
+        
+        slowP = slowP->next;
+        checkP = checkP->next;
+    }
+    return true;
+}
+
+bool isPalindrome(Node *head) {
     Node *p = head;
     int a[1000000];
-    int cnt=0;
+    int cnt = 0;
     while (p != NULL) {
         a[cnt] = p->data;
         cnt++;
         p = p->next;
     }
-    int high = cnt-1;
+    int high = cnt - 1;
     int low = 0;
     while (low < high) {
         if (a[low] == a[high]) {
             low++;
             high--;
             continue;
-        } else return false;
+        } else
+            return false;
     }
     return true;
 }
 
 int main() {
-  Node *head = NULL;
+    Node *head = NULL;
 
-  insertAtEnd(head,1);
-  insertAtEnd(head,2);
-  insertAtEnd(head,1);
-  insertAtEnd(head,2);
-  insertAtEnd(head,1);
+    insertAtEnd(head, 1);
+    insertAtEnd(head, 1);
+    insertAtEnd(head, 2);
+    insertAtEnd(head, 2);
+    insertAtEnd(head, 1);
+    insertAtEnd(head, 1);
 
-  cout << isPalindrome(head) << endl;
+    cout << isPalindromeEff(head) << endl;
 }
