02d8d6aab8c07870357ef3cd0363dfbf64f0824a
Mano Sriram <mano.sriram0@gmail.com>
2019-12-01 00:33:54 +0530

    Add fasthash32.

--- Bloom-Filter/Insert.hpp
+++ Bloom-Filter/Insert.hpp
@@ -8,12 +8,15 @@ bitset<32> Insert(const char *c) {
 
     int hashOne = jenkinHash(c);
     int hashTwo = murmur_32(c, sz, 444);
+    int hashThree = fasthash32(c, sz, 444);
 
     hashOne %= size_;
     hashTwo %= size_;
+    hashThree %= size_;
 
     bset |= (1 << (size_ - hashOne));
     bset |= (1 << (size_ - hashTwo));
+    bset |= (1 << (size_ - hashThree));
 
     return bset;
 }
