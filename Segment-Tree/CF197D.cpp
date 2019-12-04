74a35346053401f4902cba1f2d74677715caee52
Mano Sriram <mano.sriram0@gmail.com>
2019-12-04 23:46:56 +0530

    Add Update in Segment-Tree Problem.

--- Segment-Tree/CF197D.cpp
+++ Segment-Tree/CF197D.cpp
@@ -1,34 +1,58 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
+static int ht = INT_MIN;
 
-void buildTree(int *tree, int a[], int st, int ed, int treeNode) {
+void Update(int *tree, int cI, int level) {
+    if (cI == 0)
+        return;
+
+    if (level % 2 != 0)
+        tree[cI] = tree[2 * cI] | tree[2 * cI + 1];
+    else
+        tree[cI] = tree[2 * cI] ^ tree[2 * cI + 1];
+
+    Update(tree, cI/2, level-1);
+}
+
+void buildTree(int *tree, int a[], int st, int ed, int treeNode, int level) {
     if (st == ed) {
         tree[treeNode] = a[st];
         return;
     }
 
     int mid = (st + ed) / 2;
-    buildTree(tree, a, st, mid, 2 * treeNode);
-    buildTree(tree, a, mid+1, ed, 2 * treeNode + 1);
-
-    if (!(treeNode & 1))
+    buildTree(tree, a, st, mid, 2 * treeNode, level+1);
+    buildTree(tree, a, mid+1, ed, 2 * treeNode + 1, level+1);
+    if (level > ht)
+        ht = level;
+    if (level % 2 == 0)
         tree[treeNode] = tree[2 * treeNode] | tree[2 * treeNode + 1];
     else
         tree[treeNode] = tree[2 * treeNode] ^ tree[2 * treeNode + 1];
-    cout << treeNode << " " << tree[treeNode] << " ";
 }
 
 int main() {
-    int a[] = {1, 2, 3, 4, 5};
-    int n = sizeof(a)/sizeof(a[0]);
+    int n, q;
+    cin >> n >> q;
+    n = pow(2, n);
+    int a[n];
+    for (int t=0;t<n;t++)
+        cin >> a[t];
 
     int *tree = new int[(2 * n) + 1];
+    buildTree(tree, a, 0, n-1, 1, 0);
 
-    buildTree(tree, a, 0, n-1, 1);
+    int hght = ht;
+    int p, b;
+    for (int t=0;t<q;t++) {
+        ht = hght;
+        cin >> p >> b;
+        --p;
+        int in = n + p;
+        tree[in] = b;
+        Update(tree, (in)/2, ht);
 
-    return 1;
-    for (int t=1;t<2*n;t++)
-        cout << tree[t] << " ";
+        cout << tree[1] << '\n';
+    }
 }
-
--- Segment-Tree/maxRangeQuery.cpp
+++ Segment-Tree/maxRangeQuery.cpp
@@ -14,12 +14,28 @@ int maxRangeQuery(int *tree, int st, int ed, int inSt, int inEd, int treeNode) {
         return INT_MIN;
 }
 
+void Update(int *tree, int cI) {
+    if (cI == 0)
+        return;
+    tree[cI] = max(tree[2 * cI], tree[2 * cI + 1]);
+
+    Update(tree, cI/2);
+}
+
 int main() {
     int arr[] = {2, 3, 123, 555, 43, 139, -122, 332};
+    int n = sizeof(arr)/sizeof(arr[0]);
 
     int *tree = new int[32];
 
     buildMaxRangeTree(tree, arr, 0, 7, 1);
 
+    cout << maxRangeQuery(tree, 0, 7, 4, 7, 1);
+    tree[n + 7] = 1996;
+    int h = (int)(ceil(log2(n)));
+    int sz = 2 * (int)pow(2, h) - 1;
+
+    Update(tree, sz-1);
+    cout << endl;
     cout << maxRangeQuery(tree, 0, 7, 4, 7, 1);
 }
